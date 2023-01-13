a1fdcc935b139c2ef70b132aa92b58b9938e1dd3
jaeeun12 <lje007007@naver.com>
2023-01-13 20:16:43 +0900

    Add files via upload

--- 20230112/(20230112).c
+++ 20230112/(20230112).c
@@ -1,64 +1,36 @@
 #include <stdio.h>
-
-
-int init_ary(int a[5][5])
-{
-    for(int i = 0; i < 5; i++)
-    {
-        for(int j = 0; j < 5; j++)
-        {
-            a[i][j] = 0;
-        }
-    }
-}    
-
-int init_ay(int b[5][5],int a[5][5])                  
-{
-	for(int i=0; i<5; i++)
-	{
-		printf("\n");
-		for(int j=0; j<5; j++)
-		{
-			a[i][j]=b[i][j];
-			printf("%3d", a[i][j]);
-		}
-	}
-	printf("\n");
-}    
-int printf_ay(int a[5][5],int b[5][5])            
-{
-	for(int i=0; i<5; i++)
-	{
-		for(int j=0; j<5; j++)
-			b[(i+1)%5][(j+1)%5]=a[i][j];
-	}
-}
-int reinit_ary(int a[5][5])                  
-{
-    for(int i = 0; i < 5; i++)
-    {
-        for(int j = 0; j <= i; j++)
-        {
-            a[i][j] = 7;                                                                                   
-        }     
-    }
-}
-
-
+#define x 5
+#define y 5
+#define z (x+y)
 int main()
 {
-    int a[5][5]; 
-    int b[5][5];
-    
-    init_ary(a);                         
-    reinit_ary(a);
-    
+	int a[x] = {4,6,8,13,15};
+	int b[y] = {1,3,5,7,10};
+	int c[z];
+	int i = 0, j = 0, p = 0;
 
-    for(int i=0; i<=5; i++)
-	{
-        init_ay(a,b);
-        printf_ay(a,b);
-
+	while (i < x && j < y) {
+		if (a[i] < b[j]) c[p++] = a[i++];
+		else c[p++] = b[j++];
 	}
+
+	while (i < x)
+		c[p++] = a[i++];
+
+	while (j < y)
+		c[p++] = b[j++];
+
+	for (i = 0; i < x; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+
+	for (j = 0; j < y; j++)
+		printf("%d ", b[j]);
+	printf("\n");
+
+	for (p = 0; p < z; p++)
+		printf("%d ", c[p]);
+	printf("\n");
+
 	return 0;
 }
